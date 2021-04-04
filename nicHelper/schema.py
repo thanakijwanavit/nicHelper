1da980319de1f625a9a9b0385cb3547a3979a5f8
EC2 Default User <ec2-user@ip-172-16-42-147.ap-southeast-1.compute.internal>
2021-04-04 16:29:32 +0000

    updated validateUrl

--- nicHelper/schema.py
+++ nicHelper/schema.py
@@ -26,7 +26,10 @@ def validateUrl(url,input_, format_ = 'json', headers = {'Cache-Control': 'no-ca
   '''
   validates whether the input_ matches the schema in the url or not
   url: str: url of the schema
-  input_:
+  input_: the input to be validated
+  format_: str: is the format of the schema 'yaml' or 'json' (Default = 'json')
+  headers: dict: the header
+  path: str: path of the schema, if root then path='/'
   '''
   if format_ == 'yaml':
     schema = getSchemaPath(url, path = path, isYaml = True)
