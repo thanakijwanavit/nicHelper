9ec83a7bd909a3f08aa27649568422d0c632909d
EC2 Default User <ec2-user@ip-172-16-42-147.ap-southeast-1.compute.internal>
2021-04-07 08:57:39 +0000

    edited schema docs

--- nicHelper/schema.py
+++ nicHelper/schema.py
@@ -10,10 +10,10 @@ from types import SimpleNamespace
 import dpath.util
 def getSchemaPath(schemaUrl:str, path:str='/', isYaml = True):
   '''
-  get a nested schema from path
-  schemaUrl: str: url of the schema
-  path: str: path of the schema, if root then path='/'
-  isYaml: Bool: is the schema yaml (false indicates that the schema is json)
+  get a nested schema from path \n
+  schemaUrl: str: url of the schema \n
+  path: str: path of the schema, if root then path='/' \n
+  isYaml: Bool: is the schema yaml (false indicates that the schema is json), default = True
   '''
   if isYaml:
     schema = yaml.load(requests.get(schemaUrl).text, Loader= yaml.Loader)
@@ -27,7 +27,7 @@ def validateUrl(url,input_, format_ = 'json', headers = {'Cache-Control': 'no-ca
   verifies whether the input_ is valid under the schema located at path in the url \n
   url: str: url where the schema file is located \n
   input_: the input to be validated \n
-  format_: str: the format of the schema; can be 'yaml' or 'json'. default = 'json' \n
+  format_: str: the format of the schema; can be 'yaml' or 'json', default = 'json' \n
   headers: dict: dictionary of HTTP headers to send with the get request to retrieve the schema \n
   path: str: path of the schema within the file, if root then path='/'
   '''
@@ -44,7 +44,11 @@ def validateUrl(url,input_, format_ = 'json', headers = {'Cache-Control': 'no-ca
 # Cell
 typeMap = {'string': str, 'number': float, 'integer': int, 'object': dict, 'array': list, 'boolean': bool, 'null': None}
 def getTypes(schemaUrl:str, typeMap:dict=typeMap)->dict:
-  '''get python types from json schema'''
+  '''
+  get python types from json schema \n
+  schemaUrl: str: url where the schema file is located \n,
+  typeMap: dict: the dictionary that matches the key to its corresponding data type
+  '''
   r = requests.get(schemaUrl)
   s = yaml.load(r.text, Loader=yaml.FullLoader)
   properties = s['properties']
@@ -52,8 +56,11 @@ def getTypes(schemaUrl:str, typeMap:dict=typeMap)->dict:
   return dtypes
 def typeMapJsonSchema(url:str, input_:dict = {}, typeMap:dict = typeMap, defaultType=str):
   '''
-  try to map the datatype into the one specified in url of json schema.
-  if type is not found, the defaultType is used
+  try to map the datatype into the one specified in url of json schema. \n
+  if type is not found, the defaultType is used \n
+  url: str where the schema file is located \n
+  typeMap: dict: the dictionary that matches the key to its corresponding data type \n
+  defaultType: set the default type if a type is not specified
   '''
   typesDict = getTypes(url, typeMap=typeMap) # get dtype from schema url
   print(f'typesDict is: {typesDict}')
