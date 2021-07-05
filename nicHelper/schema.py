e75c29200f5453b22fa2c559ede0963eea826f55
pin pin tea-makorn <chanopsethiwan233@gmail.com>
2021-07-05 17:24:23 +0000

    fixed a silly mistake

--- nicHelper/schema.py
+++ nicHelper/schema.py
@@ -52,7 +52,7 @@ def getTypes(schemaUrl:str, typeMap:dict=typeMap)->dict:
   r = requests.get(schemaUrl)
   s = yaml.load(r.text, Loader=yaml.FullLoader)
   properties = s['properties']
-  dtypes = {k: typeMap.get(v['type']) for k,v in properties.items(), 'Unidentifiable Type'}
+  dtypes = {k: typeMap.get(v['type'], 'Unidentifiable Type') for k,v in properties.items()}
   return dtypes
 
 # Cell
